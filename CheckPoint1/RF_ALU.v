ff3212595b08c059d67116005b3c1232e13d5617
ctapiero <cristian.tapiero.1995@gmail.com>
2022-10-17 16:14:37 -0600

    added pc ALU to top module for integration

--- CheckPoint1/RF_ALU.v
+++ CheckPoint1/RF_ALU.v
@@ -13,16 +13,25 @@ module RF_ALU #(parameter WIDTH = 16, REGBITS = 4)(
     input [REGBITS-1:0] regAddress1, regAddress2,
     input [WIDTH-1:0] immediate,
     input shiftType,
+    input [WIDTH-1: 0]  pc,
+	    //Sign extend here or before passing to alu?
+	input			    jumpEN,
+	input [WIDTH-1: 0]  RTarget,
+	input               jalEN,
+    input ALUselect,
+	output [WIDTH-1: 0] Rlink,
+	output [WIDTH-1: 0] pcOut,
     output [WIDTH-1:0] result,
     output [WIDTH-1:0] pcreg,
     output [7:0] PSR
 );
     wire [WIDTH-1:0] regData1, regData2;
-    wire [WIDTH-1:0] aluResult, shiftOut;
+    wire [WIDTH-1:0] aluResult1,aluResult2,aluResult,shiftOut;
     wire [WIDTH-1:0] src1, src2;
     wire [WIDTH-1:0] ALU_Out, opOutput;
     wire [7:0] PSRresult;
-	 wire [WIDTH-1:0] writeData;
+	wire [WIDTH-1:0] writeData;
+    
     // // set register for reg1 data output
     // flopr #(WIDTH) areg(clk, reset, regData1, regData1Out);
     // // set register for reg2 data output
@@ -38,10 +47,14 @@ module RF_ALU #(parameter WIDTH = 16, REGBITS = 4)(
     // output from shifter and AlU unit
     mux2 #(WIDTH) outputMux(shiftOut, aluResult, shiftOrALU, opOutput);
     
+    // pc counter doesnt care about PSR for now
+    pcALU #(WIDTH)(src1,src2,jumpEN,RTarget,jalEN,Rlink,aluResult2);
     // Operational units
     shifter #(WIDTH) shifterUnit(src1, shiftDirection, shiftType, shiftOut);
     RegisterFile #(WIDTH, REGBITS) regFile(clk, regWrite, regAddress1, regAddress2, writeData, regData1, regData2);
-    ALU #(WIDTH) alu_unit(src1, src2, aluControl,aluResult, PSRresult);
+    ALU #(WIDTH) alu_unit(src1, src2, aluControl,aluResult1, PSRresult);
     
+    mux2 #(WIDTH) ALUmux(aluResult1, aluResult2, ALUselect, aluResult);
+
     assign writeData = opOutput;
 endmodule
